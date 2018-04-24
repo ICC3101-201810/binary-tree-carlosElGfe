39dbc0c67a3e5c2c146e414fb27784886efd2de1
erriveros <erriveros@miuandes.cl>
2018-04-24 16:25:49 -0300

    first lab april 24

---
+++ binarry tree/binarry tree/Nodo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarry_tree
+{
+    public class Arbol
+    {
+        public Type T;
+        public class Nodo<T>
+        {
+            public T info;
+            public Nodo<T> izq, der;
+            public Nodo(T minfo)
+            {
+                info = minfo;
+                this.izq = null;
+                this.der = null;
+            }
+            Nodo<T> raiz;
+            
+            public void insertarnodo(List<int> a, T info)
+            {
+                if (raiz == null)
+                {
+                    Nodo<T> nodonuevo = new Nodo<T>(info);
+                }
+                if (raiz != null)
+                {
+                    raiz[a[0]];
+                }
+            }
+        }
+        
+
+    }
+}
---
+++ binarry tree/binarry tree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarry_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenido al mundo de los arboles yo sere su guia  (>°_°)>...");
+            Nodo raiz = new Nodo(null,null,null);
+
+        }
+    }
+}
